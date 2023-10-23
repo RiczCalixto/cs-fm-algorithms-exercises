034a2f18c0740ed363a956489b6a024355e0b5a5
Ricardo Calixto <38529592+RiczCalixto@users.noreply.github.com>
2023-10-22 22:57:52 -0300

    feat: buble sort my own solution

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -11,12 +11,38 @@
 */
 
 function bubbleSort(nums) {
+  let shouldLoop = true;
+
+  while (shouldLoop) {
+    let didSwap;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > nums[i + 1]) {
+        let temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        didSwap = true;
+      }
+    }
+    if (!didSwap) {
+      shouldLoop = false;
+    }
+  }
+
+  return nums;
+
   // code goes here
+}
+
+function swapElements(array, startIndex, endIndex) {
+  let temp = array[startIndex];
+  array[startIndex] = array[endIndex];
+  array[endIndex] = temp;
+  return array;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
